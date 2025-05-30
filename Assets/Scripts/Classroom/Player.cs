dc3c89860d3e4198f30dad93f2f7a383256009e8
r4bb1t <r4bb1t.github@gmail.com>
2025-05-31 00:00:34 +0900

    Feat: Add classroom default map

--- Assets/Scripts/Classroom/Player.cs
+++ Assets/Scripts/Classroom/Player.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Teacher teacher;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        teacher = FindObjectOfType<Teacher>();
     }
 
     void Update()
@@ -18,6 +21,27 @@ public class Player : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveX, moveY).normalized;
+
+        if (teacher.isWatching && (moveX != 0 || moveY != 0))
+        {
+            if (!IsHiddenBehindCover())
+            {
+                GameOver();
+            }
+        }
+
         rb.velocity = movement * moveSpeed;
+    }
+
+    bool IsHiddenBehindCover()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 5f, LayerMask.GetMask("Cover"));
+        return hit.collider != null;
+    }
+
+
+    void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
